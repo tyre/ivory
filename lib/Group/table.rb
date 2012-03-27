a3b5e6c3da0fef2ebc3368bd9e2a57bc8811221d
Chris Maddox <tyre77@gmail.com>
2012-03-26 21:39:34 -0400

    Table updated with complete #to_html, fixed instance variable/accessor ambiguity

--- lib/Group/table.rb
+++ lib/Group/table.rb
@@ -1,5 +1,4 @@
 require "./group.rb"
-require "awesome_print"
 
 class Table < Group
   include Enumerable
@@ -8,60 +7,58 @@ class Table < Group
   attr_reader :rows
 
   def initialize
-    @rows = [["frat"]]
+    @rows = [[]]
+
   end
 
   def columns_count 
     ordered = rows.max_by { |row| row.size }
-    ap ordered
-    @columns_count = ordered.first.size
+    @columns_count = ordered
   end
 
   def each
-    rows.each do |row|
-      row.each do |cell|
+    self.rows.each do |row|
+      self.row.each do |cell|
         yield cell
       end
     end
   end
 
   def each_row
-    rows.each { |row| yield row }
+    self.rows.each { |row| yield row }
   end
 
   def each_column
-    (0...columns)
-    column = rows.collect do |row|
-      row[column]
+    (0...columns_count)
+    column = self.rows.collect do |row|
+      row
     end
     yield column
   end
  #####################################################################
-  
-  def to_html
-    html = "<tr>"
-    each_row do |row|
-      row.each do |cell|
-        html+= cell.respond_to? to_html ? cell.to_html : cell.to_s
-      end
-    end
-  end
 
-  def to_s 
-    string = horizontal_line
-    each_row do |row|
-      row.each do |cell|
-        string += "| " + cell.inspect + " |"
-        string += horizontal_line
-      end
+ def to_html
+  html = "<table>"
+  each_row do |row|
+    html += "<tr>"
+    row.each do |cell|
+      cell_html= cell.respond_to?(:to_html) ? cell.to_html : cell.to_s
+      html += "<td>#{cell_html}</td>"
     end
-    string
+    html += "</tr>"
   end
+  html += "</table>"
+end
 
-  def horizontal_line
-    horizontal = "____"
-    columns_count.times { horizontal += "__"}
-    horizontal+="\n"
-  end
+  # def to_s 
+  #   string = horizontal_line
+  #   each_row do |row|
+  #     row.each do |cell|
+  #       string += "| " + cell.inspect + " |"
+  #       string += horizontal_line
+  #     end
+  #   end
+  #   string
+  # end
 
 end
