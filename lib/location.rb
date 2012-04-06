766ebc52658ce2fb57ccb5f9d3a2bde6f88d0552
Jan Koszewski <jan.koszewski@gmail.com>
2012-04-06 16:11:04 -0400

    updating location to deploy css, js, and html elements which will fix an elements position absolutely

--- lib/location.rb
+++ lib/location.rb
@@ -7,4 +7,26 @@ class Location
   def initialize(attributes={})
     define_attributes(attributes)
   end
+
+  def to_css
+    "
+    ##{id} {
+    position: #{type};
+    }
+    "
+  end
+
+  def to_js
+    "
+    <script src='http://code.jquery.com/jquery-latest.js'></script>
+    "
+  end
+
+  def to_html
+    "
+    <script>
+    $('##{id}').offset({ top: #{top}, left: #{left} });
+    </script>
+    "
+  end
 end
